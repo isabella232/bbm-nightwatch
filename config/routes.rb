a82f7604688bbd155b03c02852d7c8741b8dc94d
Zoltan Ormandi <zoltan.ormandi@emarsys.com>
2017-11-03 11:15:33 +0100

    Set root URL to health check so that CodeShip can test if deployment was successful

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/healthcheck', to: 'health_check#index'
+
+  root to: 'health_check#index'
 end
