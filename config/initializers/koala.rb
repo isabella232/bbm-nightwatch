16b6f58a8c01520547d58fd3b2ee10b5f362fd7a
Adam Luzsi <adamluzsi@gmail.com>
2017-11-03 19:39:14 +0100

    because koala not thread safe, there have to be one single point in the system where the coala config is being constantly updated without overleaping. There fore the on per web request set is not safe or would require a mutex

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -1,8 +1,21 @@
 Koala.configure do |config|
-  config.access_token = ENV["ACCESS_TOKEN"]
-  config.app_access_token = ENV["APP_ACCESS_TOKEN"]
   config.app_id = ENV["APP_ID"]
   config.app_secret = ENV["APP_SECRET"]
 end
 
 FACEBOOK_GROUP_ID = ::ENV["FACEBOOK_GROUP_ID"] || raise('FACEBOOK_GROUP_ID ENV variable not set!')
+
+
+Thread.new do
+  loop do
+
+    Koala.configure do |config|
+      config.access_token = AccessToken.find_by(name: 'page')&.value
+      config.app_access_token = AccessToken.find_by(name: 'app')&.value
+    end
+
+    sleep(60)
+
+  end
+end
+
