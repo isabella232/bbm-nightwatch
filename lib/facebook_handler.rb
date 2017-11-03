c209a002e6643d68d55f5c28cb77b1d4c8180fa3
Adam Luzsi <adamluzsi@gmail.com>
2017-11-03 14:05:00 +0100

    facebook handler module for easy of use

--- lib/facebook_handler.rb
+++ lib/facebook_handler.rb
@@ -1,4 +1,19 @@
+require 'koala'
+require 'json'
 module FacebookHandler
-    extend(self)
+  extend(self)
 
+  def post_to_group(group_id, message)
+    graph.new.put_connections(group_id, 'feed', message: message)['id']
+  end
+
+  def update_a_post(post_id)
+    graph.graph_call(post_id, { message: 'Post modified from API' }, 'post')['success']
+  end
+
+  private
+
+  def graph
+    Koala::Facebook::API.new
+  end
 end
