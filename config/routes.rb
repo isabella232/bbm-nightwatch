06203b3434ea826ba64bfe8313ac9a883483ec6b
Tamas Drahos <tdrahos@tdrahos-mbp.local>
2017-11-03 13:16:20 +0100

    Scaffold for Donation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :donations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/healthcheck', to: 'health_check#index'
 
