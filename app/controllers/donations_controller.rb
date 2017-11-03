a66f54da04af0d810e908f47195920ef2ce710f4
Zoltan Ormandi <zoltan.ormandi@emarsys.com>
2017-11-03 14:34:10 +0100

    Hungarian localization

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -17,7 +17,7 @@ class DonationsController < ApplicationController
 
     respond_to do |format|
       if @donation.save
-        format.html { redirect_to @donation, notice: 'Donation was successfully created.' }
+        format.html { redirect_to @donation, notice: t('donation.accepted') }
         format.json { render :show, status: :created, location: @donation }
       else
         format.html { render :new }
