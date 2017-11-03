42b5a2b810a2d7c13529dd5c214764579f735e97
Adam Luzsi <adamluzsi@gmail.com>
2017-11-03 14:53:45 +0100

    facebook group id createion integration

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,3 +1,4 @@
+
 class DonationsController < ApplicationController
   before_action :set_donation, only: [:show, :edit, :update, :destroy]
 
@@ -12,8 +13,10 @@ class DonationsController < ApplicationController
     @donation = Donation.new
   end
 
+
   def create
     @donation = Donation.new(donation_params)
+    @donation.facebook_post_id = ::FacebookHandler.post_to_group(post_message_template.render(@donation)).to_s
 
     respond_to do |format|
       if @donation.save
@@ -27,6 +30,11 @@ class DonationsController < ApplicationController
   end
 
   private
+
+  def post_message_template
+    template_path = Rails.root.join("app","views","posts","group_message.erb").to_s
+    Tilt::ERBTemplate.new(template_path)
+  end
 
   def set_donation
     @donation = Donation.find(params[:id])
