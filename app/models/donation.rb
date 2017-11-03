ad866e58512494e10ffa6bdb450588a61865d639
Milán Unicsovics <u.milan@gmail.com>
2017-11-03 15:24:44 +0100

    Add available range attributes to donation model

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -3,8 +3,14 @@ class Donation < ApplicationRecord
   validates :source_address, presence: true
   validates :food_type, presence: true
   validates :information_source, presence: true
+  validates :available_from, presence: true
+  validates :available_to, presence: true
 
   validates :quantity, presence: true,
-                       numericality: {greater_than_or_equal_to: 50}
+            numericality: { greater_than_or_equal_to: 50 }
+
+  validates :not_perishable, inclusion: { in: [true] }
+
+  attr_accessor :not_perishable
 
 end
