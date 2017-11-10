17acee0ce3e63a263a9c094f417686118d3afe4e
Tamás Drahos <tamas.drahos@emarsys.com>
2017-11-10 21:36:42 +0100

    Remove unecessary lines form koala config

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -3,6 +3,6 @@ FACEBOOK_GROUP_ID = ::ENV["FACEBOOK_GROUP_ID"] || raise('FACEBOOK_GROUP_ID ENV v
 Koala.configure do |config|
   config.app_id = ENV["APP_ID"]
   config.app_secret = ENV["APP_SECRET"]
-  config.access_token = AccessToken.find_by(name: 'page')&.value
-  config.app_access_token = AccessToken.find_by(name: 'app')&.value
+  # config.access_token = AccessToken.find_by(name: 'page')&.value
+  # config.app_access_token = AccessToken.find_by(name: 'app')&.value
 end
