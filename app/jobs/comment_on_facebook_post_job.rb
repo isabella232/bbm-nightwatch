8df551daf128ca89a54329b2de626d1ebf6a3c54
Tamás Drahos <tamas.drahos@emarsys.com>
2017-11-15 20:49:29 +0100

    Add guard to CommentOnFacebookPostJob

--- app/jobs/comment_on_facebook_post_job.rb
+++ app/jobs/comment_on_facebook_post_job.rb
@@ -2,6 +2,8 @@ class CommentOnFacebookPostJob < ApplicationJob
   queue_as :facebook
 
   def perform(post_id, message)
+    return unless post_id && message
+
     ::FacebookHandler.comment_on_post post_id, message
   end
 end
