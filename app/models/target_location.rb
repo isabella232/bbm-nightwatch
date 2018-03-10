b72f4b896abf8b33c41822bcddd6df5c85386885
Tamás Drahos <tamas.drahos@emarsys.com>
2018-03-10 06:15:10 +0100

    Lose validations for TargetLocations to enable seeds of the current dataset from the google spreadsheet

--- app/models/target_location.rb
+++ app/models/target_location.rb
@@ -1,9 +1,8 @@
 class TargetLocation < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :address, presence: true
-  validates :contact_name, presence: true
-  validates :phone, presence: true
-  validates :email, presence: true,
-                    format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
-  validates :capacity, presence: true
+  # validates :contact_name, presence: true
+  # validates :phone, presence: true
+  # validates :email, presence: true, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
+  # validates :capacity, presence: true
 end
--- spec/models/target_location_spec.rb
+++ spec/models/target_location_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe TargetLocation, type: :model do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name) }
-  it { is_expected.to validate_presence_of(:phone) }
-  it { is_expected.to validate_presence_of(:email) }
-  it { is_expected.to allow_value('user@example.com').for(:email) }
-  it { is_expected.to validate_presence_of(:contact_name) }
-  it { is_expected.to validate_presence_of(:capacity) }
+  it { is_expected.to validate_presence_of(:address) }
+  # it { is_expected.to validate_presence_of(:phone) }
+  # it { is_expected.to validate_presence_of(:email) }
+  # it { is_expected.to allow_value('user@example.com').for(:email) }
+  # it { is_expected.to validate_presence_of(:contact_name) }
+  # it { is_expected.to validate_presence_of(:capacity) }
 end
