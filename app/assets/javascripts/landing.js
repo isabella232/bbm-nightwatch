21e4f9253207d77051d918976150c81e9a55cb62
Tamás Drahos <tamas.drahos@emarsys.com>
2018-07-25 09:07:39 +0200

    Add new items to success stories. Display it in a caroussel to make it not too long and visually attractive

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -6,4 +6,23 @@ $(function () {
     var anchor = $(this).attr('href');
     document.querySelector(anchor).scrollIntoView({ block: 'start', behavior: 'smooth' });
   });
+
+  $('.carousel[data-type="multi"] .item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i=0;i<1;i++) {
+      next=next.next();
+      if (!next.length) {
+      	next = $(this).siblings(':first');
+    	}
+
+      next.children(':first-child').clone().appendTo($(this));
+    }
+  });
+
+  $('[data-toggle="tooltip"]').tooltip();
 });
