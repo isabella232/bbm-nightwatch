09c45e91975fff09a6d35ea93275391a7b43b066
Milán Unicsovics <u.milan@gmail.com>
2018-10-15 16:05:07 +0200

    Fix donation mailer preview to present donation for the right user

--- spec/mailers/previews/donation_mailer_preview.rb
+++ spec/mailers/previews/donation_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/donation_mailer
 class DonationMailerPreview < ActionMailer::Preview
   def created_notification
-    DonationMailer.created_notification(Donation.first, User.first)
+    DonationMailer.created_notification(User.first.donations.first, User.first)
   end
 end
