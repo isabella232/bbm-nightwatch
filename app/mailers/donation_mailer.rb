461284c4bc2a81376ce6917fef93d13a02ddb008
stoiet <to.stoiet@gmail.com>
2018-10-15 15:59:59 +0200

    Add skeleton of revocated notification for donation mailer

--- app/mailers/donation_mailer.rb
+++ app/mailers/donation_mailer.rb
@@ -2,6 +2,12 @@ class DonationMailer < ApplicationMailer
   def created_notification(donation, user)
     @donation = donation
     @user = user
-    mail(to: @user.email, subject: I18n.t('donation.mailer.created_notification.subject'))
+    mail to: @user.email, subject: I18n.t('donation.mailer.created_notification.subject')
+  end
+
+  def revocated_notification(donation, user)
+    @donation = donation
+    @user = user
+    mail to: @user.email, subject: I18n.t('donation.mailer.revocated_notification.subject')
   end
 end
--- spec/mailers/previews/donation_mailer_preview.rb
+++ spec/mailers/previews/donation_mailer_preview.rb
@@ -3,4 +3,8 @@ class DonationMailerPreview < ActionMailer::Preview
   def created_notification
     DonationMailer.created_notification(User.first.donations.first, User.first)
   end
+
+  def revocated_notification
+    DonationMailer.revocated_notification(Donation.first, User.first)
+  end
 end
