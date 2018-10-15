78563247f906172a909544af2c20a780c52807f9
Milán Unicsovics <u.milan@gmail.com>
2018-10-15 15:17:21 +0200

    Add donation mailer preview for testing purposes

--- spec/mailers/previews/donation_mailer_preview.rb
+++ spec/mailers/previews/donation_mailer_preview.rb
@@ -1,4 +1,6 @@
-# Preview all emails at http://localhost:3000/rails/mailers/dontaion_mailer
+# Preview all emails at http://localhost:3000/rails/mailers/donation_mailer
 class DonationMailerPreview < ActionMailer::Preview
-
+  def created_notification
+    DonationMailer.created_notification(Donation.first, User.first)
+  end
 end
