d3bfff68eb506361eaba2d100f23b9a29cff9556
Milán Unicsovics <u.milan@gmail.com>
2018-10-16 14:07:24 +0200

    Notify admins about new subscribers

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -7,6 +7,7 @@ class SubscribersController < ApplicationController
     @subscriber = Subscriber.find_or_create_by subscriber_params
 
     if @subscriber.persisted?
+      notify_admins_about_subscription
       redirect_to root_path, notice: t('subscribers.create.thank_you')
     else
       render :new
@@ -14,6 +15,12 @@ class SubscribersController < ApplicationController
   end
 
   private
+
+  def notify_admins_about_subscription
+    User.admins.each do |user|
+      SubscriberMailer.new_subscription_notification(user, @subscriber).deliver_later
+    end
+  end
 
   def subscriber_params
     params.require(:subscriber).permit(:email)
--- spec/controllers/subscribers_controller_spec.rb
+++ spec/controllers/subscribers_controller_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe SubscribersController, type: :controller do
   let(:valid_attributes) { {email: 'subscriber@example.com'} }
   let(:invalid_attributes) { {email: 'not-an-email'} }
+  let(:admins) { create_list :user, 2, role: "admin" }
+  let(:non_admins) { create_list :user, 2, role: "non_admin" }
 
   describe "GET #new" do
     it "returns a success response" do
@@ -12,11 +14,20 @@ RSpec.describe SubscribersController, type: :controller do
   end
 
   describe "POST #create" do
+    subject(:create_subscriber) { post :create, params: {subscriber: valid_attributes} }
+
     context "with valid params" do
       it "creates a new Subscriber" do
-        expect {
-          post :create, params: {subscriber: valid_attributes}
-        }.to change(Subscriber, :count).by(1)
+        expect { create_subscriber }.to change(Subscriber, :count).by(1)
+      end
+
+      it "sends email notification to all users who have email notifications enabled" do
+        email = double 'email'
+        expect(email).to receive(:deliver_later).twice
+        admins.each { |u| expect(SubscriberMailer).to receive(:new_subscription_notification).with(u, anything).and_return(email) }
+        non_admins.each { |u| expect(SubscriberMailer).to_not receive(:new_subscription_notification).with(u, anything) }
+
+        create_subscriber
       end
 
       it "redirects to the root path" do
@@ -28,16 +39,16 @@ RSpec.describe SubscribersController, type: :controller do
         before { create :subscriber, email: 'subscriber@example.com' }
 
         it "does not create a new Subscriber" do
-          expect {
-            post :create, params: {subscriber: valid_attributes}
-          }.not_to change(Subscriber, :count)
+          expect { create_subscriber }.not_to change(Subscriber, :count)
         end
       end
     end
 
     context "with invalid params" do
+      let(:valid_attributes) { invalid_attributes }
+
       it "returns a success response (i.e. to display the 'new' template)" do
-        post :create, params: {subscriber: invalid_attributes}
+        create_subscriber
         expect(response).to be_successful
       end
     end
