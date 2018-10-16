fa85f58358f5be1c3351dc7127f7695f058b285d
brigi-vitalyos <brigi.vitalyos@gmail.com>
2018-10-16 15:05:04 +0200

    Change transport taken on notification email text

--- spec/mailers/transport_mailer_spec.rb
+++ spec/mailers/transport_mailer_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe TransportMailer, type: :mailer do
   describe '#transport_taken_on_notification' do
     subject(:mail) { TransportMailer.transport_taken_on_notification(transport) }
 
-    let(:user) { build :user, name: 'who makes a donation'}
+    let(:user) { build :user, name: 'who makes a donation' }
     let(:donation) { build :donation, user: user }
-    let(:transporter) { build :user, name: 'transporter'}
-    let(:transport) { build :transport, donation: donation, transporter: transporter}
+    let(:transporter) { build :user, name: 'transporter' }
+    let(:transport) { build :transport, donation: donation, transporter: transporter }
 
     it 'renders the headers' do
       expect(mail.subject).to eq(I18n.t('transport.mailer.transport_taken_on_notification.subject'))
@@ -18,12 +18,9 @@ RSpec.describe TransportMailer, type: :mailer do
       expect(mail.to).to eq([user.email])
     end
 
-    it 'renders the donation url to mail body' do
-      expect(mail.body.encoded).to match(url_for(donation))
-    end
-
     it 'renders the name of transporter to mail body' do
-      expect(mail.body.encoded).to match(transporter.name)
+      expect(mail.html_part.body).to include(transporter.name)
+      expect(mail.text_part.body).to include(transporter.name)
     end
   end
 end
