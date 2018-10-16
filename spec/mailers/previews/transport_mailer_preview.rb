2202b72127ca622b5e34c260a0709a9ca68710cb
Milán Unicsovics <u.milan@gmail.com>
2018-10-16 16:33:08 +0200

    Fix transport mailer preview

--- spec/mailers/previews/transport_mailer_preview.rb
+++ spec/mailers/previews/transport_mailer_preview.rb
@@ -1,7 +1,7 @@
 # Preview all emails at http://localhost:5100/rails/mailers/transport_mailer
 class TransportMailerPreview < ActionMailer::Preview
   def transport_cancelled_notification
-    TransportMailer.transport_cancelled_notification(User.first.transports.first, User.first)
+    TransportMailer.transport_cancelled_notification(User.first.transports.first.donation, User.first)
   end
 
   def transport_taken_on_notification
