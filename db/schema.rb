f46c0cfb2316720eac0f2814839f88fdbe9dabb6
Milán Unicsovics <u.milan@gmail.com>
2018-10-17 13:09:10 +0200

    Add name to subscribers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_19_091810) do
+ActiveRecord::Schema.define(version: 2018_10_19_091811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2018_10_19_091810) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "success_stories", force: :cascade do |t|
