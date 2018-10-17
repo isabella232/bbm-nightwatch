1fdfac683650f6f22b45b820a674129c7a22fd7e
stoiet <to.stoiet@gmail.com>
2018-10-17 13:49:37 +0200

    Add missing tests for subscriber mailer

--- spec/mailers/subscriber_mailer_spec.rb
+++ spec/mailers/subscriber_mailer_spec.rb
@@ -1,5 +1,49 @@
 require "rails_helper"
 
 RSpec.describe SubscriberMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "#new_subscription_notification" do
+    subject(:mail) { SubscriberMailer.new_subscription_notification(user, subscriber) }
+
+    let(:user) { build :user }
+    let(:subscriber) { build :subscriber }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq I18n.t('subscriber.mailer.new_subscription_notification.subject')
+    end
+
+    it "renders the receiver email" do
+      expect(mail.to).to eq [user.email]
+    end
+
+    it "renders the subscriber email to mail body" do
+      expect(mail.html_part.body).to include subscriber.email
+      expect(mail.text_part.body).to include subscriber.email
+    end
+
+    it "renders the subject to mail body" do
+      expect(mail.html_part.body).to include I18n.t('subscriber.mailer.new_subscription_notification.subject')
+      expect(mail.text_part.body).to include I18n.t('subscriber.mailer.new_subscription_notification.subject')
+    end
+
+    it "renders the subject to mail body" do
+      expect(mail.html_part.body).to include I18n.t('subscriber.mailer.new_subscription_notification.subject')
+      expect(mail.text_part.body).to include I18n.t('subscriber.mailer.new_subscription_notification.subject')
+    end
+
+    it "renders invitation text to mail body" do
+      expect(mail.html_part.body).to include I18n.t('subscriber.mailer.new_subscription_notification.send_invite')
+      expect(mail.text_part.body).to include I18n.t('subscriber.mailer.new_subscription_notification.send_invite')
+    end
+
+    it "renders invitation text to mail body" do
+      expect(mail.html_part.body).to include I18n.t('subscriber.mailer.new_subscription_notification.send_invite')
+      expect(mail.text_part.body).to include I18n.t('subscriber.mailer.new_subscription_notification.send_invite')
+    end
+
+    it "renders invitation link to mail body" do
+      expect(mail.html_part.body).to include new_user_invitation_url(subscriber)
+      expect(mail.text_part.body).to include new_user_invitation_url(subscriber)
+    end
+  end
 end
