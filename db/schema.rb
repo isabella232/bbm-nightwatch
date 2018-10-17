dfb4b808db2b8bd695821e7ba8ac6f9fb2cd402b
Milán Unicsovics <u.milan@gmail.com>
2018-10-17 11:17:08 +0200

    Update schema dump

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_18_125840) do
+ActiveRecord::Schema.define(version: 2018_10_19_091810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2018_10_18_125840) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position", default: 1, null: false
   end
 
   create_table "target_locations", force: :cascade do |t|
