a99b5c157a9e74d2a64e4beef6c7d2dde1d80b2a
Zoltan Ormandi <zoltan.ormandi@emarsys.com>
2018-10-21 16:11:11 +0200

    Formatting

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,28 +1,28 @@
 $(function () {
-  $('body.landing-page').scrollspy({target: '#navbar', offset: 50})
+    $('body.landing-page').scrollspy({target: '#navbar', offset: 50});
 
-  $('a[href^="#"]').click(function(event){
-    event.preventDefault();
-    var anchor = $(this).attr('href');
-    document.querySelector(anchor).scrollIntoView({ block: 'start', behavior: 'smooth' });
-  });
+    $('a[href^="#"]').click(function (event) {
+        event.preventDefault();
+        var anchor = $(this).attr('href');
+        document.querySelector(anchor).scrollIntoView({block: 'start', behavior: 'smooth'});
+    });
 
-  $('.carousel[data-type="multi"] .item').each(function(){
-    var next = $(this).next();
-    if (!next.length) {
-      next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
+    $('.carousel[data-type="multi"] .item').each(function () {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
 
-    for (var i=0;i<1;i++) {
-      next=next.next();
-      if (!next.length) {
-      	next = $(this).siblings(':first');
-    	}
+        for (var i = 0; i < 1; i++) {
+            next = next.next();
+            if (!next.length) {
+                next = $(this).siblings(':first');
+            }
 
-      next.children(':first-child').clone().appendTo($(this));
-    }
-  });
+            next.children(':first-child').clone().appendTo($(this));
+        }
+    });
 
-  $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="tooltip"]').tooltip();
 });
